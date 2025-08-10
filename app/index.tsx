a3fc223b296130fd635b488aa366e6059f0231d6
kevmhughes <kevhughes24@hotmail.com>
2025-08-10 20:13:13 +0200

    style: add custom font loading and update home screen styles
    Integrates Quicksand font loading in RootLayout with splash screen handling. Updates the index page with new background color, text styles, and font usage for improved appearance.

--- app/index.tsx
+++ app/index.tsx
@@ -2,9 +2,9 @@ import { Text, View } from "react-native";
  
 export default function Index() {
   return (
-    <View className="flex-1 items-center justify-center bg-green-100">
-      <Text className="text-xl font-bold text-red-100">
-        Welcome to Nativewind!
+    <View className="flex-1 items-center justify-center bg-white-100">
+      <Text className="text-5xl text-center font-bold text-primary font-quicksand-bold">
+        Welcome to my React Native App!
       </Text>
     </View>
   );
