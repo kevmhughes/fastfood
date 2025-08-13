e37888aa396345ecdaa2b6465731fa10e5a34280
kevmhughes <kevhughes24@hotmail.com>
2025-08-13 17:52:53 +0200

    layout(auth): enhance _layout.tsx with keyboard handling and login graphic

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -1,13 +1,18 @@
-import { View, Text } from 'react-native'
+import { View, KeyboardAvoidingView, Image, Platform, ScrollView, Dimensions, ImageBackground } from 'react-native'
 import React from 'react'
-import { SafeAreaView } from 'react-native-safe-area-context'
 import { Slot } from 'expo-router'
+import { images } from '@/constants'
 
 export default function _Layout() {
   return (
-    <SafeAreaView>
-      <Text>Auth Layout</Text>
+    <KeyboardAvoidingView behavior={Platform.OS === 'ios' ? 'padding' : 'height'}>
+      <ScrollView className='bg-white h-full' keyboardShouldPersistTaps="handled">
+        <View className="w-full relative" style={{ height: Dimensions.get('screen').height / 2.25}}>
+        <ImageBackground source={images.loginGraphic} className='size-full rounded-b-lg' resizeMode='stretch'/>
+        <Image source={images.logo} className='self-center size-48 absolute -bottom-16 z-10'/>
+        </View>
+        </ScrollView>
       <Slot />
-    </SafeAreaView>
+    </KeyboardAvoidingView>
   )
 }
