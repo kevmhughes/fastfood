fd9529e3224e6f677b75b1284f90440a5b4d8cff
kevmhughes <kevhughes24@hotmail.com>
2025-08-13 20:43:18 +0200

    i: add CustomInput component with focus styling and configurable props

---
+++ components/CustomInput.tsx
@@ -0,0 +1,33 @@
+import { View, Text, TextInput } from 'react-native'
+import React, { useState } from 'react'
+import cn from "clsx";
+import { CustomInputProps } from '@/type'
+
+const CustomInput = ({ 
+    placeholder = "Enter text", 
+    value, 
+    onChangeText,
+    label,
+    secureTextEntry = false,
+    keyboardType="default"}: CustomInputProps) => {
+    const [isFocused, setIsFocused] = useState(false);
+
+  return (
+    <View className='w-full '>
+      <Text className='label'>{label}</Text>
+      <TextInput 
+        autoCapitalize='none'
+        autoCorrect={false}
+        value={value}
+        secureTextEntry={secureTextEntry}
+        keyboardType={keyboardType}
+        onFocus={() => setIsFocused(true)}
+        placeholder={placeholder}
+        placeholderTextColor="#888"
+        className={cn("input", isFocused ? "border-primary" : "border-gray-300")}
+      />
+    </View>
+  )
+}
+
+export default CustomInput
