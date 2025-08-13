2480795e398028d85b197eec62bd30773fea8060
kevmhughes <kevhughes24@hotmail.com>
2025-08-13 17:52:27 +0200

    config: add Appwrite configuration for endpoint, project, and collections

--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -1,5 +1,7 @@
 export const appwriteConfig = {
   endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
-  platform: "com.company.fastfood",
   projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
+  platform: "com.company.fastfood",
+  databaseId: "689ca88b002b600c0861",
+  userCollectionId:"689ca8ce001a5c48a3d4",
 }
